56865e91173cb4e96568f3d02f948af30852f9ff
Tania Zubrienko <tatiana.zubrienko@gmail.com>
2023-11-27 11:31:57 +0100

    mware

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -8,7 +8,7 @@ const verifyToken = expressjwt({
 })
 
 function getTokenFromHeaders(req) {
-    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Baerer') {
         const token = req.headers.authorization.split(' ')[1]
         return token
     }
